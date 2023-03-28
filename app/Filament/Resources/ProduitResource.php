4c94bf0f2620809cb6675f2695723dd09e4499ae
Mambi <myfimail@yahoo.fr>
2023-03-28 10:27:52 +0100

    first commit

---
+++ app/Filament/Resources/ProduitResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProduitResource\Pages;
+use App\Filament\Resources\ProduitResource\RelationManagers;
+use App\Models\Produit;
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProduitResource extends Resource
+{
+    protected static ?string $model = Produit::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    Forms\Components\TextInput::make('name')
+                        ->required()
+                        ->maxLength(255),
+                ])
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkAction::make('bulk_updates')
+                    ->action(function ($livewire, $data, $records) {
+                        //...
+                    })
+                    ->form(
+                        function ($records, $data) {
+                            return [
+                                Forms\Components\Select::make('periode')
+                                    ->label('Période')
+                                    //->options(fn () => Periode::pluck('name', 'id'))
+                                    ->required(),
+                            ];
+                        }
+                    ),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            // VenteManager::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProduits::route('/'),
+            'create' => Pages\CreateProduit::route('/create'),
+            'edit' => Pages\EditProduit::route('/{record}/edit'),
+        ];
+    }
+}
